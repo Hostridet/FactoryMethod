157ad0290ca4bea9be3d6f162403623c8449d8aa
Никита Румянцев <rumiantcev.no@students.dvfu.ru>
2021-04-24 14:17:18 +1000

    first commit

---
+++ cmake-build-debug/CMakeFiles/factorymethod.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/factorymethod.dir/main.cpp.o"
+  "factorymethod.exe"
+  "factorymethod.pdb"
+  "libfactorymethod.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/factorymethod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
